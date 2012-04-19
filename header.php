e1d731b0bb2b40eb6df27a890dccc3a0a35295cf
Deirdre Connolly <dconnolly@hubspot.com>
2012-04-18 23:02:42 -0400

    Slides.js 2.0beta, with 'play' option, 'responsive'.

--- header.php
+++ header.php
@@ -19,19 +19,25 @@
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
   <?php if (!has_post_thumbnail() && !wp_title("", 0)) { ?>
-  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/slides.min.jquery.js"></script>
+  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/slides.js"></script>
   <script>
     $(function(){
       $("#slides").slides({
-            preload: true,
+            responsive: true,
             play: 5000,
-            slideSpeed: 800,
+            playInterval: 5000,
+            navigation: false,
             pagination: false,
-            effect: 'fade',
-            crossfade: true,
-            generatePagination: false
-      });
+            startAtSlide: 5,
+            effects: {
+                       play: "fade"
+            },
+            fade: {
+                    interval: 1000
+            }
+      }).slides("play");
     });
+    
   </script>
   <?php } ?>
 
@@ -45,72 +51,67 @@
   <!--[if lt IE 7]><p class="chromeframe">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
   <?php roots_header_before(); ?>
-    <header id="banner" class="container" role="banner">
-      <?php roots_header_inside(); ?>
-      <div class="top">
-        <h1 id="header-logo">
-          <a href="/"><?php bloginfo('name'); ?></a> <small> est. 1976</small>
-        </h1>  
-        <div class="navbar">
-          <div class="navbar-inner">
-            <div class="<?php echo WRAP_CLASSES; ?>">
-              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </a>
-              <nav id="nav-main" class="nav-collapse" role="navigation">
-                <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Navbar_Nav_Walker())); ?>
-                <form role="search" method="get" id="searchform" class="navbar-search form-search pull-right" action="<?php echo home_url('/'); ?>">
-                  <label class="visuallyhidden" for="s">
-                    <?php _e('Search for:', 'roots'); ?>
-                  </label>
-                  <input type="text" value="" name="s" id="s" class="search-query span2" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-                </form>
-              </nav>
-            </div>
-          </div>
+  <header id="banner" class="container" role="banner">
+    <?php roots_header_inside(); ?>  
+    <h1 id="header-logo">
+      <a href="/"><?php bloginfo('name'); ?></a> <small>est. 1976</small>
+    </h1>  
+    <div class="navbar">
+      <div class="navbar-inner">
+        <div class="<?php echo WRAP_CLASSES; ?>">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <nav id="nav-main" class="nav-collapse" role="navigation">
+            <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Navbar_Nav_Walker())); ?>
+            <form role="search" method="get" id="searchform" class="navbar-search form-search pull-right" action="<?php echo home_url('/'); ?>">
+              <label class="visuallyhidden" for="s">
+                <?php _e('Search for:', 'roots'); ?>
+              </label>
+              <input type="text" value="" name="s" id="s" class="search-query span2" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+            </form>
+          </nav>
         </div>
       </div>
-      
-      <?php if (!has_post_thumbnail() && !wp_title("", 0)) { ?>
-      <div id="slides">
-        <div class="slides_container">
-            <div id="support">
-              <img src="/assets/IMG_3924_960x300.jpg">
-              <h1><span>Support</span></h1>              
-            </div>
-            <div id="score">
-              <img src="/assets/KimVarney-30_960x300.jpg">
-              <h1><span>Score</span></h1>
-            </div>
-            <div id="tackle">
-              <img src="/assets/KimVarney-1_960x300.jpg">
-              <h1><span>Tackle</span></h1>
-            </div>
-            <div id="win">
-              <img src="/assets/KimVarney-9_960x300.jpg">
-              <h1><span>Win</span></h1>
-            </div>
-            <div id="attack">
-              <img src="/assets/KimVarney-28_960x3001.jpg">
-              <h1><span>Attack</span></h1>
-            </div>
-          </div>
-        </div>
-        <?php } else { ?>
-        <div class="attachment-post-thumbnail">
-          <?php if (has_post_thumbnail()) { ?>
-            <?php the_post_thumbnail(); ?>
-          <?php } else { ?>
-            <img src="/assets/jerseys.jpg" class="attachment-post-thumbnail wp-post-image" alt="Boston Women's Rugby jerseys, 2011" title="BWRFC jerseys 2011">
-          <?php } ?>
-          <?php if (wp_title("", 0)) { ?>
-            <h1><span><?php wp_title("", true); ?></span></h1>
-          <?php } ?>
-        <?php } ?>
+    </div>
+  </div>
+  <?php if (!has_post_thumbnail() && !wp_title("", 0)) { ?>
+  <div id="slides">
+      <div id="support" class="slide">
+        <img src="/assets/IMG_3924_960x300.jpg">
+        <h1><span>Support</span></h1>              
       </div>
-    </header>
+      <div id="score" class="slide">
+        <img src="/assets/KimVarney-30_960x300.jpg">
+        <h1><span>Score</span></h1>
+      </div>
+      <div id="tackle" class="slide">
+        <img src="/assets/KimVarney-1_960x300.jpg">
+        <h1><span>Tackle</span></h1>
+      </div>
+      <div id="win" class="slide">
+        <img src="/assets/KimVarney-9_960x300.jpg">
+        <h1><span>Win</span></h1>
+      </div>
+      <div id="attack" class="slide">
+        <img src="/assets/KimVarney-28_960x3001.jpg">
+        <h1><span>Attack</span></h1>
+      </div>
+  </div>
+  <?php } else { ?>
+  <div class="attachment-post-thumbnail">
+    <?php if (has_post_thumbnail()) { ?>
+    <?php the_post_thumbnail(); ?>
+    <?php } else { ?>
+    <img src="/assets/jerseys.jpg" class="attachment-post-thumbnail wp-post-image" alt="Boston Women's Rugby jerseys, 2011" title="BWRFC jerseys 2011">
+    <?php } ?>
+    <?php if (wp_title("", 0)) { ?>
+    <h1><span><?php wp_title("", true); ?></span></h1>
+    <?php } ?>
+    <?php } ?>
+  </header>
   <?php roots_header_after(); ?>
 
   <?php roots_wrap_before(); ?>
