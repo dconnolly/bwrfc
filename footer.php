b811b805a569c857af2ec11d79e7479572ac259b
Deirdre Connolly <dconnolly@hubspot.com>
2012-05-07 21:14:09 -0400

    Updating tracking code to async analytics 3.0

--- footer.php
+++ footer.php
@@ -1,18 +1,18 @@
     <?php roots_footer_before(); ?>
     
-    <footer id="content-info" class="<?php echo WRAP_CLASSES; ?>" role="contentinfo">
+    <footer id="content-info" role="contentinfo">
       <hr>
       <?php roots_footer_inside(); ?>
       <?php dynamic_sidebar('roots-footer'); ?>
       <div class="footer-sponsors">
            <a href="http://www.oneills.com" >
-             <img id="oneills-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/oneils_logo.png" alt="O'Neills logo">
+             <img id="oneills-logo" class="sponsor-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/oneils_logo.png" alt="O'Neills logo">
             </a>
            <a href="http://www.fenwayhealth.org" >
-             <img id="fenway-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/fenway_logo.png" alt="Fenway Health logo">
+             <img id="fenway-logo" class="sponsor-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/fenway_logo.png" alt="Fenway Health logo">
             </a>
             <a href="http://www.bansheeboston.com" >
-             <img id="banshee-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/banshee_logo.png" alt="The Banshee logo">
+             <img id="banshee-logo" class="sponsor-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/banshee_logo.png" alt="The Banshee logo">
             </a>
       </div>
       <h6 class="photo-credits">Photography by <a href="http://www.kimberlyvarney.com/">Kimberly Varney</a>.</h6>
@@ -20,17 +20,19 @@
     </footer>
     <?php roots_footer_after(); ?>
 
-  </div><!-- /#wrap -->
-
   <?php wp_footer(); ?>
   <?php roots_footer(); ?>
-  <!-- Start of HubSpot Logging Code  -->
-  <script type="text/javascript" language="javascript">
-      var hs_portalid=166270; 
-      var hs_salog_version = "2.00";
-      var hs_ppa = "bwrfc.app12.hubspot.com";
-      document.write(unescape("%3Cscript src='" + document.location.protocol + "//" + hs_ppa + "/salog.js.aspx' type='text/javascript'%3E%3C/script%3E"));
-  </script>
-  <!-- End of HubSpot Logging Code -->
+  </div> <!-- /div.container -->
+  <!-- Start of Async HubSpot Tracking Code -->
+    <script type="text/javascript">
+        var _hsq = _hsq || [];
+        (function(d,s,i) {
+            if (d.getElementById(i)){return;}
+            var n = d.createElement(s),e = document.getElementsByTagName(s)[0];
+            n.id=i;n.src = '//js.hubspot.com/tracking/166270.js';
+            e.parentNode.insertBefore(n, e);
+        })(document, "script", "hs-analytics");
+    </script>
+  <!-- End of Async Hubspot Tracking Code -->     
 </body>
 </html>
