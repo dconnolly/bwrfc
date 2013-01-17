945fc6a7fa726a1d22a9ccecc2d91b9bee3049c0
Deirdre Connolly <durumcrustulum@gmail.com>
2013-01-17 13:37:25 -0500

    Add gravity form snippet to Contact Us page template, bump theme version.

--- contact-us.php
+++ contact-us.php
@@ -19,30 +19,9 @@ get_header(); ?>
           <h1> Contact Us!</h1>
         </div>
         <div>
-        <span class="hs-form-target">
-          <script type="text/javascript">
-            var ckTest = document.cookie.match ( '(^|;) ?hubspotutk=([^;]*)(;|$)' ),
-            fuid = null, jqVers = null, hutk = null, jsSrc;
-            if (ckTest && ckTest[2]) {
-              hutk = ckTest[2]; 
-            } else if (window['hsut']){
-              hutk = window['hsut'];
-            }
-            if (typeof(jQuery) !== 'undefined') {
-              jqVers = jQuery().jquery;
-            }
-            jsSrc = "http://api.hubapi.com/embed/v1/forms/166270/cf0ee084-cb97-45c7-bf1f-96783dde98e5";
-            jsSrc += "?hutk=" + hutk;
-  
-            jsSrc += "&jq=" + jqVers;
-            document.write("<script src='" + jsSrc + "'><\/script>");
-          </script>
-          <script>
-            hbspt.forms.initialize(hbspt.forms.context, '.hs-form-target');
-          </script>
-        </span>
-      <?php roots_sidebar_inside_after(); ?>
-      </div>
+            [gravityform id="1" name="Contact Us" ajax="true"]
+        </div>
+        <?php roots_sidebar_inside_after(); ?>
       </aside><!-- /#sidebar -->
     <?php roots_sidebar_after(); ?>
     </div><!-- /#content -->
