3b2fc06d85a8de69f547b2fe7116f236fc4c233f
Deirdre Connolly <durumcrustulum@gmail.com>
2013-06-13 00:45:32 -0400

    Cape Cod Sevens updates to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,30 +1,29 @@
 <div class="module new-players">
     <div class="module-header">
-        <h2>Play for Boston</h2>
+	<h2>Play for Boston</h2>
     </div>
     <div class="module-body">
        <div><h3>Nationally ranked D1 club rugby</h3></div>
        <a href="/cape-cod-sevens">
-           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/new-players-ruck-warmup.jpg" alt="Play for BWRFC, ruck with the best">
+	   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/new-players-ruck-warmup.jpg" alt="Play for BWRFC, ruck with the best">
        </a>
     </div>
     <div class="module-footer">
-        <h6><a href="/team/new-players"><i data-icon="&#xe13c;"></i>Get in touch</a></h6>
+	<h6><a href="/team/new-players"><i data-icon="&#xe13c;"></i>Get in touch</a></h6>
     </div>
 </div>
 
 <div class="module cape-cod-sevens">
     <div class="module-header">
-        <h2>Cape Cod Sevens</h2>
+	<h2>Cape Cod Sevens</h2>
     </div>
     <div class="module-body">
        <div><h3>Disney dreams come alive on July 13th</h3></div>
        <a href="/cape-cod-sevens">
-           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cape-cod-sevens-tutus.jpg" alt="Cape Cod Sevens, with tutus">
+	   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/CC7sLogo.jpg" alt="Cape Cod Sevens, now with more Disney">
        </a>
     </div>
     <div class="module-footer">
-        <h6><a href="/cape-cod-sevens"><i data-icon="&#xe13c;"></i>Prepare for battle</a></h6>
+	<h6><a href="/cape-cod-sevens"><i data-icon="&#xe13c;"></i>When you wish upon a star...</a></h6>
     </div>
 </div>
-
