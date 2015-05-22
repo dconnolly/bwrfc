f2849e055db759326a0826f2a6007781da67158e
Deirdre Connolly <durumcrustulum@gmail.com>
2015-05-22 00:31:57 -0400

    Generalize sidebar copy

--- sidebar.php
+++ sidebar.php
@@ -3,13 +3,12 @@
 	<h2>Play for Boston</h2>
     </div>
     <div class="module-body">
-       <div><h3>Nationally ranked D1 club rugby</h3></div>
        <a href="/team/new-players">
 	   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/new-players-ruck-warmup.jpg" alt="Play for BWRFC, ruck with the best">
        </a>
     </div>
     <div class="module-footer">
-	<h6><a href="/team/new-players"><i data-icon="&#xe13c;"></i>Get in touch</a></h6>
+	<h6><a href="/team/new-players">▶ Join Us</a></h6>
     </div>
 </div>
 
@@ -18,12 +17,11 @@
 	<h2>Cape Cod Sevens</h2>
     </div>
     <div class="module-body">
-       <div><h3>Congrats to our 2013 champions</h3></div>
        <a href="/cape-cod-sevens">
-	   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cape-cod-sevens-tutus.jpg" alt="Cape Cod Sevens, now with more Disney">
+         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cape-cod-sevens-tutus.jpg" alt="Cape Cod Sevens, now with more Disney">
        </a>
     </div>
     <div class="module-footer">
-	<h6><a href="/cape-cod-sevens"><i data-icon="&#xe13c;"></i>When you wish upon a star...</a></h6>
+	<h6><a href="/cape-cod-sevens"><▶ Get ready</a></h6>
     </div>
 </div>
